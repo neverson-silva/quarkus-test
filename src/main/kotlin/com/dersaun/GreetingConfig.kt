fe1dbcf7cbe3763646329763112bfb68d98f33a2
Neverson Silva <neverson@Neversons-MacBook-Pro.local>
2025-10-25 16:16:01 -0300

    first commit

---
+++ src/main/kotlin/com/dersaun/GreetingConfig.kt
@@ -0,0 +1,13 @@
+package com.dersaun
+
+import io.smallrye.config.ConfigMapping
+import io.smallrye.config.WithName
+
+
+@ConfigMapping(prefix = "greeting")
+interface GreetingConfig {
+
+    @WithName("message")
+    fun message(): String?
+
+}
